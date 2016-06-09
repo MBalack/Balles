addac5dd511679b05a37ce73d64b562eca2a1278
MBalack <ipemyeu@gmail.com>
2016-06-09 11:11:50 +0700

    :dancer:

--- Ezreal7/Program.cs
+++ Ezreal7/Program.cs
@@ -482,7 +482,7 @@ namespace Ezreal7
         {
             var KsQ = KillStealMenu["KsQ"].Cast<CheckBox>().CurrentValue;
             var KsW = KillStealMenu["KsW"].Cast<CheckBox>().CurrentValue;
-            foreach (var target in EntityManager.Heroes.Enemies.Where(hero => hero.IsValidTarget(1200) && !hero.IsDead && !hero.IsZombie && (hero.HealthPercent <= 25)))
+            foreach (var target in EntityManager.Heroes.Enemies.Where(hero => hero.IsValidTarget(1200) && !hero.HasBuff("JudicatorIntervention") && !hero.HasBuff("kindredrnodeathbuff") && !hero.IsDead && !hero.IsZombie && (hero.HealthPercent <= 25)))
             {
                 if (KsQ && Q.IsReady() && target.IsValidTarget(Q.Range))
                 {
