bb10cbffb7803c0fd27fe6a986a5e876685c3ab3
MBalack <ipemyeu@gmail.com>
2016-06-17 22:36:03 +0700

    Added Noti

--- Bistana/Program.cs
+++ Bistana/Program.cs
@@ -268,7 +268,7 @@ namespace Bristana
                 {
                     if (Player.Instance.GetSpellDamage(target, SpellSlot.R) > target.Health)
                     {
-                        Drawing.DrawText(Drawing.Width * 0.1f, Drawing.Height * 0.5f, Color.Red, (int)(target.Health / Player.Instance.GetSpellDamage(target, SpellSlot.R)) + " Useeeeee RRRRRRRR Cannnnnnnn Killlllllllll: " + target.ChampionName);
+                        Drawing.DrawText(Drawing.Width * 0.1f, Drawing.Height * 0.5f, Color.Red, " Useeeeee RRRRRRRR Cannnnnnnn Killlllllllll: " + target.ChampionName);
                     }
                 }
             }
