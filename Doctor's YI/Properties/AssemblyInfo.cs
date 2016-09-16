8f310375e2c6506e42a6f98f4b3bfed1d88650e2
MBalack <ipemyeu@gmail.com>
2016-09-16 19:10:47 +0700

    Big

--- Doctor's YI/Properties/AssemblyInfo.cs
+++ Doctor's YI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Doctor's YI")]
+[assembly: AssemblyTitle("Doctor's Yi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Doctor's YI")]
+[assembly: AssemblyProduct("Doctor's Yi")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
